6da0397ae5821e931da47cee8f0e054975a79206
Josafe <josafebalili@gmail.com>
2024-02-25 19:25:51 +0800

    Add email and email duplication validation

--- app/code/Thecrud/Note/Controller/Adminhtml/TakeNote/Save.php
+++ app/code/Thecrud/Note/Controller/Adminhtml/TakeNote/Save.php
@@ -6,6 +6,7 @@ namespace Thecrud\Note\Controller\Adminhtml\TakeNote;
 
 use Thecrud\Note\Model\TakeNoteFactory;
 use Thecrud\Note\Model\ResourceModel\TakeNote as TakeNoteResource;
+use Thecrud\Note\Model\ResourceModel\TakeNote\CollectionFactory as TakeNoteCollectionFactory;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Action\HttpPostActionInterface;
@@ -14,19 +15,25 @@ use Magento\Framework\Exception\LocalizedException;
 
 class Save extends Action implements HttpPostActionInterface
 {
+    protected $takeNoteCollectionFactory;
+
     public function __construct(
         Context $context,
         TakeNoteResource $resource,
-        TakeNoteFactory $takeNoteFactory
+        TakeNoteFactory $takeNoteFactory,
+        TakeNoteCollectionFactory $takeNoteCollectionFactory
     ) {
         parent::__construct($context);
         $this->resource = $resource;
         $this->takeNoteFactory = $takeNoteFactory; 
+        $this->takeNoteCollectionFactory = $takeNoteCollectionFactory;
     }
 
     public function execute(): ResultInterface
     {
         $data = $this->getRequest()->getPostValue();
+
+        $email = $data['email'];
 
         $resultRedirect = $this->resultRedirectFactory->create();
         if ($data) {
@@ -35,16 +42,25 @@ class Save extends Action implements HttpPostActionInterface
                 $data['entity_id'] = null;
             }
 
-            $model->setData($data);
+            $existingEmailCount = $this->takeNoteCollectionFactory->create()
+                ->addFieldToFilter('email', $email)
+                ->getSize();
 
-            try {
-                $this->resource->save($model);
-                $this->messageManager->addSuccessMessage(__('Saved.'));
-                return $resultRedirect->setPath('*/*/');
-            } catch (LocalizedException $exception) {
-                $this->messageManager->addExceptionMessage($exception);
-            } catch (\Throwable $e) {
-               $this->messageManager->addErrorMessage(__('Something went wrong while saving.'));
+            if ($existingEmailCount > 0) {
+                $this->messageManager->addErrorMessage(__('Email already sent a note'));
+            
+            } else {
+
+            $model->setData($data);
+                try {
+                    $this->resource->save($model);
+                    $this->messageManager->addSuccessMessage(__('Saved.'));
+                    return $resultRedirect->setPath('*/*/');
+                } catch (LocalizedException $exception) {
+                    $this->messageManager->addExceptionMessage($exception);
+                } catch (\Throwable $e) {
+                $this->messageManager->addErrorMessage(__('Something went wrong while saving.'));
+                }
             }
         }
 
